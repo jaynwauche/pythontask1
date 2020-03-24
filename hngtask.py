d35d93cf4ed11996b3365c379d87397d92e915dd
Nwauche Junior Chimenka <juniornwauche@gmail.com>
2020-03-24 19:59:53 +0000

    first python task

---
+++ hngtask.py
@@ -0,0 +1,7 @@
+# A Basic calculator for Area of a circle
+pie = 3.142
+radius = input("Input value of radius:")
+convert_radius = float(radius)
+Area_of_circle = pie*(convert_radius**2)
+ans = Area_of_circle
+print(f'Area of the circle is {ans}')
